b71af85b89881289c27fbdd48864d75e4d07044a
bloodofspring <t.shagin@gmail.com>
2026-01-07 19:11:51 +0300

    fix: Доекомпозирован InteractiveNameCardv2.tsx

---
+++ app/src/pages/home/ui/name-card-v2/InteractiveNameCardv2.constants.ts
@@ -0,0 +1,7 @@
+// Гарани
+export const facesConfig = [
+  { id: 1 },
+  { id: 2, origin: '0 100%', transform: 'rotate(-60deg) rotatex(-109.5deg)' },
+  { id: 3, origin: '100% 100%', transform: 'rotate(60deg) rotatex(-109.5deg)' },
+  { id: 4, origin: '50% 100%', transform: 'rotate(180deg) rotatex(-109.5deg)' },
+]
